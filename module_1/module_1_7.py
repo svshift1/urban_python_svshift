70de9832185e3154e6c4c529ad8307ba7364601b
svshift1 <svshift@gmail.com>
2024-08-08 06:44:48 +0300

    fixed

--- module_1/module_1_7.py
+++ module_1/module_1_7.py
@@ -2,10 +2,13 @@ grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 
 # ну так:
+# students = list(students)
+# students.sort()
 # res = dict(zip(students,[sum(x)/len(x) for x in grades]))
 
 # или:
 students = list(students)
+students.sort()
 res = {}
 # нет, пять раз копировать одну строчку не буду
 for i in range(len(students)):
