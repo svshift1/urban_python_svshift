8c4498bf731efbd6480d0fefe7f7d0609010909f
svshift1 <svshift@gmail.com>
2024-08-15 20:30:13 +0300

    module2_3

--- module_2/module_2_2.py
+++ module_2/module_2_2.py
@@ -1,10 +1,9 @@
-
-first=int(input())
-second=int(input())
-third=int(input())
-if first==second and second==third: # Если все числа равны между собой, то вывести 3
+first = int(input())
+second = int(input())
+third = int(input())
+if first == second and second == third:  # Если все числа равны между собой, то вывести 3
     print(3)
-elif first==second or second==third or first==third: # Если хотя бы 2 из 3 введённых чисел равны между собой, то вывести 2
+elif first == second or second == third or first == third:  # Если хотя бы 2 из 3 введённых чисел равны между собой, то вывести 2
     print(2)
-else : # Если равных чисел среди 3-х вообще нет, то вывести 0
+else:  # Если равных чисел среди 3-х вообще нет, то вывести 0
     print(0)
