bdd42c9ba0f9d0318c2965b51ea9c5953aa36b0d
svshift1 <svshift@gmail.com>
2024-08-15 20:29:14 +0300

    module2_3

---
+++ module_2/module_2_3.py
@@ -0,0 +1,7 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+while i < len(my_list) and my_list[i] >= 0:
+    if my_list[i] > 0:
+        print(my_list[i])
+    i += 1
+    # не хочу break и continue тут использовать
