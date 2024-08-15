2831f1a4704bb1138fffe6ba764a6b2c7b011156
svshift1 <svshift@gmail.com>
2024-08-15 23:49:59 +0300

    module2_4

--- module_2/module_2_4.py
+++ module_2/module_2_4.py
@@ -2,11 +2,14 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 for x in numbers:
+    is_prime = True
     for k in range(2, x // 2 + 1):
         if x % k == 0:
-            not_primes.append(x)
+            is_prime = False
             break
-    else:
+    if is_prime:
         primes.append(x)
+    else:
+        not_primes.append(x)
 print('Primes:', primes)
 print('Not Primes:', not_primes)
