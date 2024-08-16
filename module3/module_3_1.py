76a29e870f4ed193f04ba2f62e62b232a949660c
svshift1 <svshift@gmail.com>
2024-08-16 12:16:58 +0300

    module3_1

---
+++ module3/module_3_1.py
@@ -0,0 +1,34 @@
+from collections.abc import Iterable
+
+
+def count_calls() -> int:
+    global calls
+    calls += 1
+    return calls
+
+
+def string_info(s: str) -> tuple:
+    count_calls()
+    return len(s), s.upper(), s.lower()
+
+
+# извините. изучаю новое для себя -- typehints.
+def is_contains(s: str, lst: Iterable) -> bool:
+    count_calls()
+    for x in lst:
+        if issubclass(type(x), str):
+            # в документации говорится что casefold в контексте case_insensitive сравнения
+            # универсальнее upper или lower. в пример приводятся европейские языки
+            # типа греческого или немецкого где разным строчным буквам может соответсвовать
+            # одна заглавная или сдвоенная
+            if s.casefold() == x.casefold():
+                return True
+    return False
+
+
+calls: int = 0
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
