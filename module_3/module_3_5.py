642711014a5d6ad16eba011ea20881974bdcf7fe
svshift1 <svshift@gmail.com>
2024-11-07 23:44:23 +0300

    module3_5

--- module_3/module_3_5.py
+++ module_3/module_3_5.py
@@ -1,14 +1,15 @@
 def get_multiplied_digits(num):
-    n=int(num)
-    s=str(n) # убрали ведущие нули если были в num если это была строка
-    if  (len(s) == 1 and int(s) == 0):
-        # последний ноль
+    n = int(num)
+    s = str(n)  # убрали ведущие нули если были в num если это была строка
+    if int(s) == 0:
+        # последний ноль(и)
         return 1
     elif len(s) == 1 and int(s) != 0:
         # последний символ числа не ноль
         return int(s[0])
     else:
-        return int(s[0])*get_multiplied_digits(int(s[1:]))
+        return int(s[0]) * get_multiplied_digits(int(s[1:]))
+
 
 result = get_multiplied_digits(40203)
 print(result)
@@ -18,4 +19,3 @@ print(result)
 
 result = get_multiplied_digits('00402030')
 print(result)
-
