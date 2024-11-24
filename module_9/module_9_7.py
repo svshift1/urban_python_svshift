329173ad5968b3311816ae088c249f0ea1278a2b
svshift1 <svshift@gmail.com>
2024-11-24 18:12:42 +0300

    module8 7

--- module_9/module_9_7.py
+++ module_9/module_9_7.py
@@ -2,18 +2,20 @@ from collections.abc import Callable
 
 
 def isprime(func: Callable) -> Callable:
-
     def check_prime(*args):
-        res=func(*args)
+        res = func(*args)
         if not isinstance(res, int):
             raise TypeError('Результат должен быть целым')
         if res <= 0:
             raise ValueError('Число должно быть натуральным')
-        if res > 1 and not any(res % i == 0 for i in range(2, res // 2)):
+        if res == 1:
+            print("Это единица")
+        elif res > 1 and not any(res % i == 0 for i in range(2, res // 2)):
             print("Простое")
         else:
             print("Составное")
         return res
+
     return check_prime
 
 
