eabadcbaa33230a6319a47284d7238ee4634406e
svshift1 <svshift@gmail.com>
2024-11-27 21:11:01 +0300

    module12_1

--- module_12/module_12_1/test_12_1.py
+++ module_12/module_12_1/test_12_1.py
@@ -1,25 +1,29 @@
-from unittest import TestCase
+import unittest
 from runner import Runner
 
-class RunnerTest(TestCase):
+
+class RunnerTest(unittest.TestCase):
 
     def test_walk(self):
-        r=Runner("vasya")
+        r = Runner("vasya")
         for i in range(10):
             r.walk()
-        self.assertEqual(r.distance,50)
+        self.assertEqual(r.distance, 50)
 
     def test_run(self):
-        r=Runner("vasya")
+        r = Runner("vasya")
         for i in range(10):
             r.run()
-        self.assertEqual(r.distance,100)
+        self.assertEqual(r.distance, 100)
 
     def test_challlenge(self):
-        r1=Runner('vasya')
-        r2=Runner('petya')
+        r1 = Runner('vasya')
+        r2 = Runner('petya')
         for i in range(10):
             r1.walk()
             r1.run()
         self.assertNotEqual(r1.distance, r2.distance)
 
+
+if __name__ == '__main__':
+    unittest.main()
