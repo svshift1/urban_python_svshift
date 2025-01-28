6e7ae087e0cfd59a4deb0c77b1297d0371874e6c
svshift1 <svshift@gmail.com>
2025-01-28 18:49:47 +0300

    module13_1

---
+++ module_13/module_13_1.py
@@ -0,0 +1,23 @@
+import asyncio
+
+
+async def start_strongman(name: str, power: float) -> None:
+    print(f"силач {name} начал соревнования!")
+    for i in range(5):
+        await asyncio.sleep(power)
+        print(f"силач {name} поднял {i}-й шар!")
+    print(f"силач {name} всё !")
+
+
+async def start_tournament(*args) -> None:
+    if len(args) % 2 != 0 or len(args) == 0:
+        raise Exception("аргументы должно быть в формате (name, power, name, power, ...)")
+    N = len(args) // 2  # к-во силачей
+    tasks = []
+    for k in range(N):
+        tasks.append(asyncio.create_task(start_strongman(args[2 * k], args[2 * k + 1])))
+    for t in tasks:
+        await t
+
+
+asyncio.run(start_tournament('Petya', 2, 'Vasya', 3, 'Kolya', 4))
